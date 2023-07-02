17450d62f94be81f096035a24cd3f9f208ccda4a
PSILVA <131625628+PAndrade22@users.noreply.github.com>
2023-07-01 22:12:16 -0300

    UordiOfiUorcefiti.cs
    Prova realizada na Universidade do Estado da Bahia, da disciplina de Jogos Digitais. Simulando um jogo entre Herói da Aliança e da Horda. Usando OOP para desenvolver esse código.

--- UordiOfiUorcefiti.cs
+++ UordiOfiUorcefiti.cs
@@ -1,5 +1,4 @@
 # UOU
-//Prova realizada na Universidade do Estado da Bahia, da disciplina de Jogos Digitais. Simulando um jogo entre Herói da Aliança e da Horda. Usando OOP para desenvolver esse código.
 
 
 
